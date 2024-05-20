ac84da4627e55d581e19cf9dc190d1228e6b1279
Nicolas SABAS <nicolas.sabas-externe@icade.fr>
2024-05-20 02:06:37 +0200

    config image field easyadmin

---
+++ src/Controller/Admin/Field/TheMDBImageField.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller\Admin\Field;
+
+use EasyCorp\Bundle\EasyAdminBundle\Contracts\Field\FieldInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Dto\FieldDto;
+use EasyCorp\Bundle\EasyAdminBundle\Field\FieldTrait;
+
+final class TheMDBImageField implements FieldInterface
+{
+    use FieldTrait;
+
+    public static function new(string $propertyName, ?string $label = null)
+    {
+        return (new self())
+            ->setProperty($propertyName)
+            ->setLabel($label)
+            ->setTemplatePath('admin/field/themdbimage-field.html.twig')
+        ;
+    }
+
+}
