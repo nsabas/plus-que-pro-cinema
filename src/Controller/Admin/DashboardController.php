1eadcb0c0b09d3ccad244029a31790ace8bbfe80
Nicolas SABAS <nicolas.sabas-externe@icade.fr>
2024-05-20 02:07:34 +0200

    config dashboard action

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Movie;
+use App\Repository\MovieRepository;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -10,26 +12,34 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class DashboardController extends AbstractDashboardController
 {
+    public function __construct(
+        private MovieRepository $movieRepository
+    )
+    {
+    }
+
     #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
-//        return parent::index();
+        $topTrendingMovies = $this->movieRepository->findAll();
 
-        // Option 1. You can make your dashboard redirect to some common page of your backend
-        //
-        // $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
-        // return $this->redirect($adminUrlGenerator->setController(OneOfYourCrudController::class)->generateUrl());
+         return $this->render(
+             'admin/dashboard/dashboard.html.twig',
+             [
+                 'movies' => $topTrendingMovies
+             ]
+         );
+    }
 
-        // Option 2. You can make your dashboard redirect to different pages depending on the user
-        //
-        // if ('jane' === $this->getUser()->getUsername()) {
-        //     return $this->redirect('...');
-        // }
-
-        // Option 3. You can render some custom template to display a proper dashboard with widgets, etc.
-        // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
-        //
-         return $this->render('admin/dashboard/dashboard.html.twig');
+    #[Route('/admin/movie/{id}', name: 'movie.details')]
+    public function movieDetails(Movie $movie): Response
+    {
+        return $this->render(
+            'admin/dashboard/movies/details.html.twig',
+            [
+                'movie' => $movie
+            ]
+        );
     }
 
     public function configureDashboard(): Dashboard
@@ -41,6 +51,6 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        yield MenuItem::linkToCrud('Movies', 'fas fa-list', Movie::class);
     }
 }
