e2022b4e8e6a552c28e0b209427f963dbc9026f7
Nicolas SABAS <nicolas.sabas-externe@icade.fr>
2024-05-20 02:08:53 +0200

    add command

---
+++ src/Command/TrendingMovieLoadCommand.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Command;
+
+use App\Entity\Movie;
+use App\Manager\RequestManager;
+use App\ServicePath\MovieRequest;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\ProgressBar;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+#[AsCommand(
+    name: 'app:trending:movie:load',
+    description: 'Add a short description for your command',
+)]
+class TrendingMovieLoadCommand extends Command
+{
+    private RequestManager $requestManager;
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(
+        RequestManager $requestManager,
+        EntityManagerInterface $entityManager
+    )
+    {
+        parent::__construct();
+        $this->requestManager = $requestManager;
+        $this->entityManager = $entityManager;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        try {
+            $topTrendingMovies = $this->requestManager->getResource(
+                MovieRequest::GET_MOVIE_TOP_TRENDING_WEEK,
+                [
+                    'query' => ['page' => 1],
+                ]
+            );
+
+            $progressBar = new ProgressBar($output, count($topTrendingMovies->results));
+
+            $results = $topTrendingMovies->results;
+
+            $progressBar->start();
+
+            foreach ($results as $movie) {
+                $movieDetails = $this->requestManager->getResource(
+                    MovieRequest::GET_MOVIE_DETAILS,
+                    [],
+                    [
+                        '{movie_id}' => $movie->id,
+                    ]
+                );
+
+                $newMovie = new Movie($movie->title);
+                $newMovie->setDescription($movieDetails->overview);
+                $newMovie->setPosterUrl($movieDetails->posterPath);
+
+                $this->entityManager->persist($newMovie);
+                $progressBar->advance();
+            }
+            $this->entityManager->flush();
+        } catch (\Exception $e) {
+            // log exception
+            dd($e);
+            return Command::FAILURE;
+        }
+
+//        $io = new SymfonyStyle($input, $output);
+//        $arg1 = $input->getArgument('arg1');
+//
+//        if ($arg1) {
+//            $io->note(sprintf('You passed an argument: %s', $arg1));
+//        }
+//
+//        if ($input->getOption('option1')) {
+//            // ...
+//        }
+//
+//        $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
+
+        return Command::SUCCESS;
+    }
+}
